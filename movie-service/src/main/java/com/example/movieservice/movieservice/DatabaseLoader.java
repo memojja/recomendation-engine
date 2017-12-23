b97a3eb502f4014212521cf770c2e545242a6233
mehmet <memojja1907@gmail.com>
2017-12-23 19:23:40 +0300

    comment out database loader.returning images by movie id

--- movie-service/src/main/java/com/example/movieservice/movieservice/DatabaseLoader.java
+++ movie-service/src/main/java/com/example/movieservice/movieservice/DatabaseLoader.java
@@ -13,7 +13,14 @@ import org.springframework.http.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
+import java.nio.file.AccessDeniedException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +39,8 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
+
+        /*
 
         SparkConf conf = new SparkConf();
         conf.setAppName("Spark MultipleContest Test");
@@ -75,13 +84,18 @@ public class DatabaseLoader implements CommandLineRunner {
             for (int a = 0; a <lastMovieList.size(); a++) {
                 if(movieRDD.get(i).getMovieId().equals(lastMovieList.get(a).getMovieId()  )){
                     movieRDD.get(i).setTmdbId(lastMovieList.get(a).getTmdbId());
+                    movieRDD.get(i).setImdbId(lastMovieList.get(a).getImdbId());
                     System.out.println("[[[[[["+movieRDD.get(i).getTmdbId());
                     isActive=true;
                 }
             }
-            if (!isActive)  movieRDD.get(i).setTmdbId("********");
+            if (!isActive){
+                movieRDD.get(i).setTmdbId("********");
+                movieRDD.get(i).setImdbId("********");
+                movieRDD.get(i).setUrl("********");
+            }
     //           if save it make comment out
-    //           movieRepository.save(movieRDD.get(i));
+//               movieRepository.save(movieRDD.get(i));
         }
 
 
@@ -91,8 +105,8 @@ public class DatabaseLoader implements CommandLineRunner {
         List<Movie> movies = movieRepository.findAll();
         List<MovieDTO> movieDTOS  = new ArrayList<>();
 
-        for (int i = 0; i < movies.size(); i++) {
-            if((!movies.get(i).getTmdbId().equals("********"))) {
+        for (int i = 3616; i < movies.size(); i++) {
+            if((!movies.get(i).getTmdbId().equals("********")) && !movies.get(i).getTmdbId().equals("999999999") ) {
                 url = "https://api.themoviedb.org/3/movie/" + movies.get(i).getTmdbId() + "?api_key=a44bfe5ae077fd38027c9c495a03e853";
                 final HttpHeaders httpHeaders = new HttpHeaders();
                 httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -104,6 +118,13 @@ public class DatabaseLoader implements CommandLineRunner {
                         MovieDTO.class);
                 System.out.println(i + "[" + movieRDD.get(i).getTmdbId() + "]" + resultGoMonth);
                 movieDTOS.add(resultGoMonth.getBody());
+
+                movies.get(i).setUrl(resultGoMonth.getBody().getPoster_path());
+
+
+
+                movieRepository.save(movies);
+
                 System.out.println(i + " " + resultGoMonth.getBody());
 
                 try {
@@ -112,12 +133,39 @@ public class DatabaseLoader implements CommandLineRunner {
                     e.printStackTrace();
                 }
 
+
+
+
+                    String imageUrl = "https://image.tmdb.org/t/p/w1280" +movies.get(i).getUrl();
+
+                    String destinationFilePath = "images/"+resultGoMonth.getBody().getPoster_path().substring(1,resultGoMonth.getBody().getPoster_path().length()); // For windows something like c:\\path\to\file\test.jpg
+
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = new URL(imageUrl).openStream();
+                        Files.copy(inputStream, Paths.get(destinationFilePath));
+                    } catch (IOException e) {
+                        System.out.println("Exception Occurred " + e);
+                    } finally {
+                        if (inputStream != null) {
+                            try {
+                                inputStream.close();
+                            } catch (IOException e) {
+                                // Ignore
+                            }
+                        }
+                    }
+
+
             }
         }
 
 
 
 
+*/
+
     }
+
 
 }
--- movie-service/src/main/java/com/example/movieservice/movieservice/controller/MovieController.java
+++ movie-service/src/main/java/com/example/movieservice/movieservice/controller/MovieController.java
@@ -3,19 +3,20 @@ package com.example.movieservice.movieservice.controller;
 import com.example.movieservice.movieservice.model.Movie;
 import com.example.movieservice.movieservice.model.movie.MovieDTO;
 import com.example.movieservice.movieservice.repository.MovieRepository;
+import org.apache.commons.io.IOUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,9 +44,23 @@ public class MovieController implements Serializable {
 
     @GetMapping(value = {"/","/home","/index"})
     public String index(){
-
-
         return "Hello World!";
     }
 
+    @GetMapping(value = "/{id}/image",produces = MediaType.IMAGE_JPEG_VALUE )
+    @ResponseBody
+    public ResponseEntity<byte[]> getMovieImageById(@PathVariable("id")Long id) throws IOException {
+        String filename="images"+movieRepository.findOne(id).getUrl();
+        InputStream inputImage = new FileInputStream(filename);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[512];
+        int l = inputImage.read(buffer);
+        while(l >= 0) {
+            outputStream.write(buffer, 0, l);
+            l = inputImage.read(buffer);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "image/png");
+        return new ResponseEntity<byte[]>(outputStream.toByteArray(), headers, HttpStatus.OK);
+    }
 }
